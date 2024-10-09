139beefe6b8862a463bda388eb3dd5f08efed91b
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2024-10-10 00:05:41 +0300

    index commit path

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,10 @@
-console.log("JS IS LIADED")
+const router = async () => {
+    const routes = [
+        {path: "/", view:()=> console.log('Viewing Dashboard')},
+        {path: "/posts", view:()=> console.log('Viewing Posts')},
+        {path: "/settings", view:()=> console.log('Viewing Settings')},
+
+
+    ]
+
+}
