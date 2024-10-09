5708948fb288b411750c3f6a6f34c960ec86e87e
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2024-10-10 00:28:25 +0300

    routes commit

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,9 @@
+
+const navigateTo= url =>{
+    history.pushState(null,null,url);
+    router();
+}
+
 const router = async () => {
     const routes = [
         {path: "/", view:()=> console.log('Viewing Dashboard')},
@@ -7,4 +13,31 @@ const router = async () => {
 
     ]
 
+// Test each route for potential match
+const potentialMatches=routes.map(route=>{
+    return{
+        route:route,
+        isMatch: location.pathname===route.path
+    };
+});
+// console.log(potentialMatches)
+let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+if(!match){
+    match={
+        route: routes[0],
+        isMatch:true
+    }
 }
+console.log(match.route.view())
+};
+window.addEventListener("popstate", router);
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.body.addEventListener("click", e => {
+    if(e.target.matches("[data-link]")){
+        e.preventDefault();
+        navigateTo(e.target.href);
+    }
+    })
+    router();
+})
